facec5f848d0e58be9b5a44f75782de415499d41
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-03-14 19:32:23 +0300

    add cats detail

--- dogcollector/main_app/models.py
+++ dogcollector/main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Dog(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    image = models.ImageField(upload_to = 'main_app/static/uploads/', max_length=100, default= 'dogs')
--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Dog
 
 # Create your views here.
 
-class Dogs:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+# class Dogs:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
-dogs = {
-    Dogs('Fido', 'German Shepherd', 'long coat', 3),
-    Dogs('Bella', 'Husky', 'thick coat', 5),
-    Dogs('Daisy', 'Golden Retriever', 'brown coat', 1)
-}
+# dogs = {
+#     Dogs('Fido', 'German Shepherd', 'long coat', 3),
+#     Dogs('Bella', 'Husky', 'thick coat', 5),
+#     Dogs('Daisy', 'Golden Retriever', 'brown coat', 1)
+# }
 
 def home(request):
     return render(request, 'home.html')
@@ -23,4 +24,9 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {'dogs': dogs})
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id = dog_id)
+    return render(request, 'dogs/detail.html', {'dog': dog})
